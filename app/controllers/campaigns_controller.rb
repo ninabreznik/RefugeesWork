9a0bb95bcf6ccb29c3eb1f26f2b2942490d8c93d
Nina Breznik <ninabreznik@gmail.com>
2014-07-27 05:51:18 +0200

    Create and edit campaign

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,16 +1,50 @@
-iclass CampaignsController < ApplicationController
+class CampaignsController < ApplicationController
   
   def index
+    @user = current_user
+    @campaigns = Campaign.all
   end 
 
   def show
+    @campaign = Campaign.find_by_id(params[:id])
   end
 
   def new
     @campaign = Campaign.new
   end
 
-  def edit
+  def create
+    @campaign = current_user.campaigns.create(:title => params[:campaign][:title],:location => params[:campaign][:location])
+
+    respond_to do |format|
+      if @campaign.save
+        format.html { redirect_to @campaign, notice: 'Campaign was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @campaign }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @campaign.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  def edit
+    @campaign = Campaign.find_by_id(params[:id])
+  end
+
+  def update
+    @user = Campaign.find(params[:id])
+    if @campaign.update_attributes(campaign_params)
+      #flash[:success] = "Campaign updated"
+      redirect_to @campaign
+    else
+      render 'edit'
+    end
+  end
+
+   private
+
+    def campaign_params
+      params.require(:campaign).permit(:title, :location)
+    end
   
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
+  private
+
+  def correct_user
+    @user = User.find_by_id(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+end
