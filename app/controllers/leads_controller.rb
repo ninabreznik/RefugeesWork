a87fae1eaa50eb21e22d55600c1536f7c3e91cae
Nina Breznik <ninabreznik@gmail.com>
2014-08-17 05:17:48 +0200

    Modify leads index and controller

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -3,25 +3,25 @@ class LeadsController < ApplicationController
     @leads = Lead.all
     @campaign = Campaign.find_by_id(params[:id])
     @lead = Lead.find_by_id(params[:id])
-  
-    coowned_ids = []
-    current_user.relationships.each do |r|
-      coowned_ids << r.coowned_id
-    end
-    current_user_campaigns = Campaign.where(id: coowned_ids)
-    
-    current_user_campaigns_locations_and_title = []
-    current_user_campaigns.each do |c|
-      current_user_campaigns_locations_and_title << [c.location, c.title]
-    end
-
-    @current_user_leads = []
-    lead_values = []
-    @leads.each do |l|
-      lead_values << [l.location,l.business_type]
-      current_user_campaigns_locations_and_title.each do |c|
-        if c == lead_values
-          @current_user_leads << l
+    if user_signed_in?
+      #checking which relationships exist for current user (which campaigns he applied for) 
+      #& finding right leads
+      coowned_ids = []
+      current_user.relationships.each do |r|
+        coowned_ids << r.coowned_id
+      end
+      current_user_campaigns = Campaign.where(id: coowned_ids)
+      current_user_campaigns_locations_and_title = []
+      current_user_campaigns.each do |c|
+        current_user_campaigns_locations_and_title << [c.location, c.title]
+      end
+      @current_user_leads = []
+      @leads.each do |l|
+        current_user_lead_locations_and_business_type = [l.location,l.business_type]
+        current_user_campaigns_locations_and_title.each do |c|
+          if c == current_user_lead_locations_and_business_type
+            @current_user_leads << l
+          end
         end
       end
     end
@@ -33,14 +33,37 @@ class LeadsController < ApplicationController
 
   def new
     @lead = Lead.new
-    @business_types = [["Painting", "Painting"], ["Fasades", "Fasades"], ["Renovations", "Renovations"], ["Mechanical Installations", "Mechanical Installations"], ["Electrical Installations", "Electrical Installations"], ["Knauf", "Knauf"], ["Ceramics", "Ceramics"], ["Parquet", "Parquet"], ["Roofing", "Roofing"], ["Masonry", "Masonry"]]
-    @time = [["As soon as possible"], ["In the next 14 days"], ["In 1-3 months"], ["Flexible"]]
+    @business_types = [
+      ["Painting", "Painting"], 
+      ["Fasades", "Fasades"], 
+      ["Renovations", "Renovations"],
+      ["Mechanical Installations", "Mechanical Installations"],
+      ["Electrical Installations", "Electrical Installations"], 
+      ["Knauf", "Knauf"],
+      ["Ceramics", "Ceramics"], 
+      ["Parquet", "Parquet"], 
+      ["Roofing", "Roofing"], 
+      ["Masonry", "Masonry"]
+    ]
+    @time = [
+      ["As soon as possible"], 
+      ["In the next 14 days"], 
+      ["In 1-3 months"], 
+      ["Flexible"]
+    ]
   end
 
   def create
-    @lead = Lead.create(:name => params[:lead][:name], :email => params[:lead][:email], :description => params[:lead][:description], 
-                         :zip => params[:lead][:zip], :phone => params[:lead][:phone], :business_type => params[:lead][:business_type], 
-                         :time => params[:lead][:time], :location => params[:lead][:location])
+    @lead = Lead.create(
+      :name           => params[:lead][:name], 
+      :email          => params[:lead][:email], 
+      :description    => params[:lead][:description], 
+      :zip            => params[:lead][:zip], 
+      :phone          => params[:lead][:phone], 
+      :business_type  => params[:lead][:business_type], 
+      :time           => params[:lead][:time], 
+      :location       => params[:lead][:location]
+    )
     redirect_to @lead
   end 
   
@@ -55,7 +78,16 @@ class LeadsController < ApplicationController
   private
 
   def campaign_params
-    params.require(:lead).permit(:name, :email, :description, :zip, :phone, :business_type, :time, :location)
+    params.require(:lead).permit(
+      :name, 
+      :email, 
+      :description, 
+      :zip, 
+      :phone, 
+      :business_type, 
+      :time, 
+      :location
+    )
   end
 
 
