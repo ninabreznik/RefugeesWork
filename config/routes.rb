6ef8f4703d82b6ee9a98231dfc639e90954d0214
Nina Breznik <ninabreznik@gmail.com>
2014-08-22 00:46:03 +0200

    Add payment options: 2Checkout ad Paypal and fix static pages: pricing and bank_transfer with routes

--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,7 @@ Rails.application.routes.draw do
   get '/pricing' => 'static_pages#pricing'
   get '/about' => 'static_pages#about'
   get '/dashboard' => 'static_pages#all_campaigns'
-
+  get '/bank_transfer' => 'static_pages#bank_transfer'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
