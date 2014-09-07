0d5890ced9e8b97274fb15e49be72e6327fb3f3e
Nina Breznik <ninabreznik@gmail.com>
2014-09-08 01:00:46 +0200

    Add translations for Leads/Index, & partly Leads/show. Add load YAML to single_page_app.html.erb

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -10,24 +10,24 @@ class Lead < ActiveRecord::Base
   has_many :selectors, through: :reverse_orders, source: :selector
 
 
-def assign_location_from_zip
-  if self.zip > 999 && self.zip < 1999
-    self.location = "Ljubljana"
-  elsif self.zip > 1999 && self.zip < 2999
-    self.location = "Maribor"
-  elsif self.zip > 2999 && self.zip < 3999
-    self.location = "Celje"
-  elsif self.zip > 3999 && self.zip < 4999
-    self.location = "Kranj" 
-  elsif self.zip > 4999 && self.zip < 5999
-    self.location = "Nova Gorica"
-  elsif self.zip > 5999 && self.zip < 6999
-    self.location = "Koper"
-  elsif self.zip > 7999 && self.zip < 8999
-    self.location = "Novo mesto"
-  elsif self.zip > 8999 && self.zip < 10000
-    self.location = "Murska Sobota"                   
-  end 
-end
+  def assign_location_from_zip
+    if self.zip > 999 && self.zip < 1999
+      self.location = "Ljubljana"
+    elsif self.zip > 1999 && self.zip < 2999
+      self.location = "Maribor"
+    elsif self.zip > 2999 && self.zip < 3999
+      self.location = "Celje"
+    elsif self.zip > 3999 && self.zip < 4999
+      self.location = "Kranj" 
+    elsif self.zip > 4999 && self.zip < 5999
+      self.location = "Nova Gorica"
+    elsif self.zip > 5999 && self.zip < 6999
+      self.location = "Koper"
+    elsif self.zip > 7999 && self.zip < 8999
+      self.location = "Novo mesto"
+    elsif self.zip > 8999 && self.zip < 10000
+      self.location = "Murska Sobota"                   
+    end 
+  end
 
 end
