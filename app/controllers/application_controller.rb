7ee578c79cc728c34d2ef02898b4b54604a3a151
Nina Breznik <ninabreznik@gmail.com>
2014-10-08 22:28:24 +0200

    Devise views - login, logout, reset password, edit profile etc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,37 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+# after_filter :store_location
+
+# def store_location
+#   # store last url - this is needed for post-login redirect to whatever the user last visited.
+#   return unless request.get? 
+#   if (request.path != "/users/sign_in" &&
+#       request.path != "/users/sign_up" &&
+#       request.path != "/users/password/new" &&
+#       request.path != "/users/password/edit" &&
+#       request.path != "/users/confirmation" &&
+#       request.path != "/users/sign_out" &&
+#       !request.xhr?) # don't store ajax calls
+#     session[:previous_url] = request.fullpath 
+#   end
+# end
+
+def after_sign_in_path_for(resource)
+  root_path
+end
+
+def after_sign_up_path_for(resource)
+  root_path
+end
+
+def after_update_path_for(resource)
+  root_path
+end
+
+def after_cancel_user_registration_path_for(resource)
+  root_path
+end
 
 end
 
