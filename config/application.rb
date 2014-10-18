33477ddf006411eb71963466f1325935471a5485
Nina Breznik <ninabreznik@gmail.com>
2014-10-18 02:15:41 +0200

    Change application.rb

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,19 @@ Bundler.require(*Rails.groups)
 
 module LeadShareApp
   class Application < Rails::Application
+    initializer 'setup_asset_pipeline', :group => :all  do |app|
+    # We don't want the default of everything that isn't js or css, because it pulls too many things in
+    app.config.assets.precompile.shift
+
+    # Explicitly register the extensions we are interested in compiling
+        app.config.assets.precompile.push(Proc.new do |path|
+        File.extname(path).in? [
+        '.html', '.erb', '.haml',                 # Templates
+        '.png',  '.gif', '.jpg', '.jpeg',         # Images
+        '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+        ]
+        end)
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
