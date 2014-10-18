c8afdb3bafbe25bc22bc06849968849c0046dfb8
Nina Breznik <ninabreznik@gmail.com>
2014-10-18 03:19:36 +0200

    Changes in secrets.yml and deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,7 +43,7 @@ set :unicorn_config_path, "config/unicorn.rb"
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.1.3'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_prefix, "RAILS_ENV=production RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
