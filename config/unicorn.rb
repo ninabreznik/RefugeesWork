b063a8eee60d3a835045c9d5f79469bd26e3e245
Nina Breznik <ninabreznik@gmail.com>
2014-10-24 15:00:50 +0200

    Fix unicorn stuff for deployment

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,42 +1,46 @@
-# config/unicorn.rb
+working_directory "/opt/leadshareapp/current"
+pid "/opt/leadshareapp/current/tmp/pids/unicorn.pid"
+stderr_path "/opt/leadshareapp/shared/log/unicorn.log"
+stdout_path "/opt/leadshareapp/shared/log/unicorn.log"
 
-if ENV['RAILS_ENV'] == 'development' or not ENV['RAILS_ENV']
+listen "/tmp/unicorn.leadshareapp.sock", :backlog => 64
+listen 3334, :tcp_nopush => true
 
-  # development
-  worker_processes 1
-  listen 3000
-  preload_app true
+worker_processes "6".to_i
+timeout 30
 
-else
+preload_app true
 
-  # production and staging
-  worker_processes 3
-  timeout 30
-  preload_app true
-
+before_exec do |server|
+ ENV["BUNDLE_GEMFILE"] = "/opt/leadshareapp/current/Gemfile"
 end
 
-
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+ # Disconnect since the database connection will not carry over
+ if defined? ActiveRecord::Base
+   ActiveRecord::Base.connection.disconnect!
+ end
 
-  # Comment out if running workers in separate processes via Procfile
-  if defined? Sidekiq
-    @sidekiq_pid ||= spawn("bundle exec sidekiq -C config/sidekiq.yml")
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+ # Quit the old unicorn process
+ old_pid = "#{server.config[:pid]}.oldbin"
+ if old_pid != server.pid
+   begin
+     sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+     Process.kill(sig, File.read(old_pid).to_i)
+   rescue Errno::ENOENT, Errno::ESRCH
+   end
+ end
+ #
+ # Throttle the master from forking too quickly by sleeping.  Due
+ # to the implementation of standard Unix signal handlers, this
+ # helps (but does not completely) prevent identical, repeated signals
+ # from being lost when the receiving process is busy.
+ sleep 1
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+ # Start up the database connection again in the worker
+ if defined?(ActiveRecord::Base)
+   ActiveRecord::Base.establish_connection
+ end
 end
