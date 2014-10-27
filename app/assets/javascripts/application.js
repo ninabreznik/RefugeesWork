4c81513185856e4d2120fbaa9d75640db22d10c6
Nina Breznik <ninabreznik@gmail.com>
2014-10-27 20:37:14 +0100

    Try longer path on application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require jquery.min.js
-//= require jquery.wookmark.js
+//= require wookmark/jquery/dist/jquery.min.js
+//= require wookmark/jquery.wookmark.js
