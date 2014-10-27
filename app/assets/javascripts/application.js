bb13b90cfe889ad329a31b954ed789eeb2dc055c
Nina Breznik <ninabreznik@gmail.com>
2014-10-27 20:25:43 +0100

    Remove require wookmark and just add require_self

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require_self
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require jquery.min.js
-//= require jquery.wookmark.js
