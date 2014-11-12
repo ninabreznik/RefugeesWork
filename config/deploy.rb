598bfd01f8d0413b1d5dc88f0e55f11b67a2c083
Nina Breznik <ninabreznik@gmail.com>
2014-11-13 00:58:53 +0100

    Styling rupture breakpoints

--- config/deploy.rb
+++ config/deploy.rb
@@ -58,13 +58,13 @@ namespace :deploy do
 
   after :publishing, :restart
 
-  # after :restart, :clear_cache do
-  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
-  #     # Here we can do anything such as:
-  #     # within release_path do
-  #     #   execute :rake, 'cache:clear'
-  #     # end
-  #   end
-  # end
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
 
 end
