f33510092eac13d45312810c4fd28349559304c0
Nina Breznik <ninabreznik@gmail.com>
2015-09-11 00:35:52 +0200

    Modify Leads/index and Mailer for new lead

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,9 +9,9 @@ Rails.application.configure do
     password:             'GnUQAUKHxIO7YpSGcY2Lzg',
     authentication:       'plain',
     enable_starttls_auto: true  }
-    
+
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'http://leadshareapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://refugeeswork.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
