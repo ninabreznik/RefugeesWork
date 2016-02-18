7d452fb570238a9814a8cabe0108596536318e61
ninabreznik <ninabreznik@gmail.com>
2016-02-18 20:30:51 +0100

    Add unsubscribe

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,25 +8,6 @@ class UserMailer < ActionMailer::Base
     mail(to: @user.email, subject: 'Welcome to RefugeesWork')
   end
 
-  def terms_of_use(user)
-    @user = user
-    mail(to: @user.email, subject: 'Uspešno ste potrdili dogovor o sodelovanju')
-  end
-
-  def affiliation_agreement(user)
-    @user = user
-    @tracking_id = @user.tracking_id
-    #@url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Uspešno ste potrdili dogovor o afiliaciji')
-  end
-
-  def affiliation_code(user)
-    @user = user
-    @user_tracking_id = @user.tracking_id
-    @user_tracking_link = leads_new_path(id: @user_tracking_id)
-    mail(to: @user.email, subject: 'Uspešno ste ustvarili afiliacijsko kodo')
-  end
-
   def new_lead(lead, user)
     @lead = lead
     @user = user
