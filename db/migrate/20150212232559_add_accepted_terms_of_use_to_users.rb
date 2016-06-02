68031180a533b5ac483cb7a8d32705a07ef062e8
David Garcia <david.garcia.mora@gmail.com>
2016-06-02 18:50:14 +0200

    Use the correct field in the migration

--- db/migrate/20150212232559_add_accepted_terms_of_use_to_users.rb
+++ db/migrate/20150212232559_add_accepted_terms_of_use_to_users.rb
@@ -1,5 +1,5 @@
 class AddAcceptedTermsOfUseToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :country, :string
+    add_column :users, :accepted_terms_of_use, :boolean
   end
 end
