af6cfe6504babaa34325e006b075f7b03521d9c2
David Garcia <david.garcia.mora@gmail.com>
2016-06-06 18:03:28 +0200

    feat: Add test for locales

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+ApplicationController.class_eval do
+  def any_action
+    render nothing: true
+  end
+end
+
+Rails.application.routes.draw do
+  get 'any_action' => 'application#any_action'
+end
+
+class ApplicationControllerTest < ActionController::TestCase
+  test 'it falls back to english without a locale' do
+    get :any_action
+    assert_response :success
+    assert_equal 'Welcome to REFUGEES WORK', I18n.t('static_pages.welcome.title')
+  end
+
+  test 'the passed locale is used for translation' do
+    get :any_action, locale: 'es'
+    assert_response :success
+    assert_equal 'Bienvenido a REFUGEES WORK', I18n.t('static_pages.welcome.title')
+  end
+
+  test 'locale is mantained between requests' do
+    skip('Not implemented due to https://github.com/rspec/rspec-rails/issues/255')
+  end
+
+  test 'if the locale does not exist it falls back to english' do
+    get :any_action, locale: 'ru'
+    assert_response :success
+    assert_equal 'Welcome to REFUGEES WORK', I18n.t('static_pages.welcome.title')
+  end
+end
