7530b773117c6febf409794bdc0f8b7dede0e9e2
David Garcia <david.garcia.mora@gmail.com>
2016-06-20 15:19:49 +0200

    Use only one count, remove unnecessary if and leads

--- test/controllers/leads_controller_test.rb
+++ test/controllers/leads_controller_test.rb
@@ -2,12 +2,16 @@ require 'test_helper'
 
 class LeadsControllerTest < ActionController::TestCase
 
-  test "should get index" do
-    create_list(:lead, 2)
+  test 'assigns leads and counts correctly' do
+    create_list(:lead, 2, address: 'Germany - Berlin')
     create(:lead, address: 'United Kingdom')
+
     get :index
+
     assert_response :success
-    assert_equal 2, assigns(:count_for_berlin)
-    assert_equal 1, assigns(:count_for_uk)
+    assert_equal 1, assigns(:addresses_count)['United Kingdom']
+    assert_equal 2, assigns(:addresses_count)['Germany - Berlin']
+    assert_equal 0, assigns(:addresses_count)['Mars']
+    assert_not_nil assigns(:sorted_leads)
   end
 end
