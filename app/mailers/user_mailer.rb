5bccf50f98536b34fb1ab9eaec90a4e4ba1a75ef
ninabreznik <ninabreznik@gmail.com>
2017-01-12 20:03:54 +0700

    Half fixed mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,20 +8,19 @@ class UserMailer < ActionMailer::Base
     mail(to: @user.email, subject: 'Welcome to RefugeesWork')
   end
 
-  def self.new_lead(lead)
+  def self.send_new_lead(lead)
     @lead = lead
-    lead_business_type = lead.business_type
-    @right_users = User.all.where(subscribed_to_notifications: true)
-    if @right_users
+    @right_users = User.all.where(subscribed_to_notifications:true)
+    if @right_users.count > 0
       @right_users.each do |right_user|
-        new_lead(right_user, lead).deliver  # SEND new_lead.html.erb to the right users
+        new_lead(right_user).deliver  # SEND new_lead.html.erb to the right users (business type)
+      end
     end
   end
 
-def new_lead(right_user, lead)
-  @lead_description = lead.description
-  mail(to: right_user.email, subject: 'New job')
-end
+  def new_lead(right_user)
+    mail(to: right_user.email, subject: 'New job')
+  end
 
   def new_order(order)
     @order = order
