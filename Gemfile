3349f59180e1af1767122e528b7ece79ad7bbe27
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2019-08-31 01:33:17 +0800

    Add email confirmation & alert components

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ group :development do
   # speeds up development by keeping your application running in the background.
   # Read more: https://github.com/rails/spring
   gem 'spring'
+  gem "letter_opener"
 end
 
 #Dashing related stuff
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624231305) do
+ActiveRecord::Schema.define(version: 20190830155457) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -106,8 +106,13 @@ ActiveRecord::Schema.define(version: 20160624231305) do
     t.string   "languages"
     t.boolean  "subscribed_to_notifications", default: true
     t.string   "user_type"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
